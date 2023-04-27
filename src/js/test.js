677d6f5494616123638140f44c41cbb4fa0f82f0
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2023-04-27 18:02:56 -0400

    script

--- src/js/test.js
+++ src/js/test.js
@@ -2,13 +2,13 @@ window.addEventListener("load",iniciar)
 
 function iniciar(){
     let intentos = 6;
-    let palabra = "hola";
+    let palabra = "";
     let apiDic = "https://clientes.api.greenborn.com.ar/public-random-word?c=9&l=5";
 
     fetch(apiDic)
         .then(response => response.json())
         .then((data)=>{
-            let dat = dat[0];
+            let dat = data[0];
             palabra = dat.toUpperCase();
             console.log(palabra);
         })
@@ -16,6 +16,17 @@ function iniciar(){
     const BOTON = document.getElementById("guess-button");
     const INPUT = document.getElementById("guess-input");
     const ERROR = document.getElementById("error");
-
     
+    function intentar(){
+        console.log(INPUT.value.toUpperCase());
+        let ingresado = INPUT.value.toUpperCase();
+        
+        
+    }
+
+    BOTON.addEventListener("click",intentar);
+    INPUT.addEventListener("click",()=>{
+        ERROR.style.display = "none";
+    }) 
+
 }//iniciar
